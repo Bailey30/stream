d89523dbc6e5628d280e6974df260ac09cabd053
Bailey30 <baileymk@hotmail.com>
2025-07-25 10:40:45 +0100

    websocket url env

--- src/server/main.js
+++ src/server/main.js
@@ -1,10 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
 import { createServer } from "node:http";
 import ViteExpress from "vite-express";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 import ServerSocket from "../server/serverSocket.js";
 
+dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
